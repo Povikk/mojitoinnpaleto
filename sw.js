cf135c83802355333d7b687eb4358ec439a7a3b8
Povik <34871770+Povikk@users.noreply.github.com>
2026-08-14 19:21:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mojito-inn-pwa-v29';
+const CACHE_NAME='mojito-inn-pwa-v30';
 const APP_SHELL=['./','./index.html','./style.css','./app.js','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./tropical-bar.webp'];
 
 self.addEventListener('install',event=>{
