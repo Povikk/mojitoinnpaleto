9a3615c761bc54d5183718f5fd2377deabab0b30
Povik <34871770+Povikk@users.noreply.github.com>
2026-08-23 14:29:30 +0200

    Met à jour sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='mojito-inn-pwa-v104';
+const CACHE_NAME='mojito-inn-pwa-v106';
 const APP_SHELL=['./','./index.html','./style.css','./app.js','./config.js','./webm-muxer.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./tropical-bar.webp','./podium-template.mp4'];
 
 self.addEventListener('install',event=>{
